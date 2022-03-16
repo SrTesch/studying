c3ad70e3aa01d7320b37fa7ea65d2c782f31779d
pedro tesch <pedrotesch.pt@gmail.com>
2022-03-16 14:02:42 -0300

    tentei a A

--- p2progA.c
+++ p2progA.c
@@ -40,10 +40,96 @@ Exemplo 1: entrada
 Exemplo 1: saída
 4 4 1 4 4 2 2
 
+10 4
+3 2 5 4
+3 8 9 10
+2 10 2
+5 1 3 6 7 8
 */
 
+
+
 #include <stdio.h>
+#include <stdlib.h>
+
+void verificaAlcance(int totalComunidades,int **comunidade, int totalUsuarios,int *verif, int *totalSoma, int i){
+	int j, k;
+	if(verif[i] == 1 && totalSoma[i] != 0){
+		return; 
+	}
+	//verif[i] = 1;
+	for(j = 0; j < totalComunidades; j++){
+		if(comunidade[j][i] == 1){
+			for(k = 0; k < totalUsuarios; k++){
+				if(comunidade[j][k] == 1){
+					if(totalSoma[k] != 0){
+						totalSoma[i] = totalSoma[k];
+						return;
+					}
+					if(k!= i && verif[k] != 1){
+						verificaAlcance(totalComunidades, comunidade, totalUsuarios, verif, totalSoma, k);
+					}
+					verif[k] = 1;
+				}
+			}
+		}
+	}
+}
+
+int atualizaAlcance(int totalComunidades,int **comunidade, int totalUsuarios, int *totalSoma, int i){
+	int k, j, maior, aux;
+	for(j = 0; j < totalComunidades; j++){
+		if(comunidade[j][i] == 1){
+			for(k = 0; k < totalUsuarios; k++){
+				if( comunidade[j][k] == 1){
+						aux = atualizaAlcance(totalComunidades, comunidade, totalUsuarios, totalSoma, k);
+						maior = (totalSoma[k] > aux)? totalSoma[k] : aux;
+					if(maior > totalSoma[i]){
+						printf("%d %d %d", i, aux, totalSoma);
+						totalSoma[k] = maior;
+					}
+				}
+			}
+		}
+	}
+	return maior;
+}
+
 int main(){
-	int n, m;	
+	int totalUsuarios, totalComunidades, usuarios, **comunidade, i, j, k, soma, *totalSoma;
+	scanf("%d %d", &totalUsuarios, &totalComunidades);
+	int *verif;
+	verif = malloc(totalUsuarios * sizeof(int));
+	totalSoma = malloc(totalUsuarios * sizeof(int));
+	comunidade = malloc (totalComunidades * sizeof(int*));
+	for(i=0; i < totalComunidades; i++){
+		comunidade[i] = malloc(totalUsuarios * sizeof(int));
+		scanf("%d", &usuarios);
+		for(j = 0; j < totalUsuarios; j++){
+			comunidade[i][j] = 0;
+		}
+		int aux;
+		for (j = 0; j < usuarios ; j++){
+			scanf("%d", &aux);
+			comunidade[i][aux-1] = 1;
+		}
+	}
+	for(i = 0; i < totalUsuarios; i++){
+		for(j=0; j < totalUsuarios; j++){
+			if(i == 0){
+				totalSoma[j] = 0;
+			}
+			verif[j] = 0;
+		}
+		verificaAlcance(totalComunidades, comunidade, totalUsuarios, verif,totalSoma, i);
+		verif[i] = 1;
+		if(totalSoma[i]== 0){
+			for(j = 0; j < totalUsuarios; j++){
+				totalSoma[i] += verif[j];
+			}
+		}
+		atualizaAlcance(totalComunidades, comunidade, totalUsuarios, totalSoma, i);
+		printf("%d ", totalSoma[i]);
+	}
 return 0;
 }
