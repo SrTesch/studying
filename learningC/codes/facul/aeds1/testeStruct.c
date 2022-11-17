a0b316d6eef53f9050d8363445b4dd3f15c256f4
pedro tesch <pedrotesch.pt@gmail.com>
2022-11-16 21:49:47 -0300

    malloc e free adicionados no código

--- learningC/codes/facul/aeds1/testeStruct.c
+++ learningC/codes/facul/aeds1/testeStruct.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 struct ponto {
-        float x;
-        float y;
+    float x;
+    float y;
 };
 
 
 void imprime(struct ponto *pp){
-    printf("Ponto: (%.2f, %.2f)\n", pp->x, pp->y);
+    printf("Ponto: (%.1f, %.1f)\n", pp->x, pp->y);
 }
 
+void captura(struct ponto *pp){
+    printf("Digite as coordenadas do ponto: \n");
+    scanf("%f %f", &pp->x, &pp->y);
+}
 
 int main(){
 
     struct ponto *pp;
-    pp->x = 10.0;
-    pp->y = 5.0;
+    pp = (struct ponto*) malloc(sizeof(struct ponto));
+    captura(pp);
 
     printf("The x value is: %.1f\n", pp->x);
     printf("The y value is: %.1f\n", pp->y);
     printf("The x address is: %p\n", &pp->x);
-    printf("The y value is: %p\n", &pp->y);
+    printf("The y address is: %p\n", &pp->y);
 
     imprime(pp);
-
+    free(pp);
     return 0;
 }
