19eac02f7eca69c6ec9715f6c27352fe2cbb03dd
pedro tesch <pedrotesch.pt@gmail.com>
2022-11-24 16:57:18 -0300

    comecei no codecademy pra ruby

---
+++ learningRuby/codeCademy/initialsAgain.rb
@@ -0,0 +1,9 @@
+my_num =     25
+
+my_boolean =     true
+
+my_string =     'Ruby'
+
+p my_num
+p my_boolean
+p my_string
---
+++ learningRuby/codeCademy/puts&print.rb
@@ -0,0 +1,7 @@
+#Print you already know, but puts is slightly different: it adds a new (blank) line after the thing you want it to print.
+
+print 'Hello'
+
+puts 'Hello'
+
+print 'Hello'
