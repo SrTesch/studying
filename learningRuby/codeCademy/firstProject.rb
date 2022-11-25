eba8e761bba251bf086d4ed7f25d66647d5a1c72
pedro tesch <pedrotesch.pt@gmail.com>
2022-11-25 17:23:35 -0300

    ...

---
+++ learningRuby/codeCademy/firstProject.rb
@@ -0,0 +1,15 @@
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize
+
+print "What city are you from? "
+city = gets.chomp
+city.capitalize
+
+print "What state/province are you from? "
+state = gets.chomp
+state.capitalize
